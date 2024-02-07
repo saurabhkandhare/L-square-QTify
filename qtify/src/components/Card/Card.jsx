51ce4b22fbe58e9e8662fac05cd78172bb1571d1
Saurabh Kandhare <spkandhare@gmail.com>
2024-02-07 20:35:05 +0530

    Sixth Commit

--- qtify/src/components/Card/Card.jsx
+++ qtify/src/components/Card/Card.jsx
@@ -1,7 +1,68 @@
+// import React from "react";
+// import styles from "./Card.module.css";
+// import { Chip, Tooltip } from "@mui/material";
+// import { Link } from "react-router-dom";
+
+// function Card({data, type}) {
+//     const getCard = (type) => {
+//         switch (type){
+//             case "album" :{
+//                 const {image, title, follows, slug, songs} = data;
+//                 return (
+//                     <Tooltip title={`${songs.length} songs`} placement="top" arrow>
+//                     <Link to={`/album/${slug}`}>
+//                     <div className={styles.wrapper}>
+//                         <div className={styles.card}> 
+//                             <img src={image} alt="album" loading="lazy"/>
+//                             <div className={styles.banner}>
+//                                 <Chip 
+//                                 label={`${follows} Follows`}
+//                                 size="small"
+//                                 className={styles.chip}
+//                                 />
+//                                 </div>
+//                                 </div>
+//                                 <div className={styles.titleWrapper}>
+//                                     <p>{title}</p>
+//                                 </div>
+//                     </div>
+//                 </Link>
+//             </Tooltip>
+//                 )
+//             }
+//             case "song" :{
+//                 const {likes, image, title} = data;
+//                 return (
+//                     <div className={styles.wrapper}>
+//                         <div className={styles.card}>
+//                             <img src={image} alt="song" loading="lazy"/>
+//                             <div className={styles.banner}>
+//                                 <div className={styles.pill}>
+//                                     <p>{likes} Likes</p>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                         <div className={styles.titleWrapper}>
+//                             <p>{title}</p>
+//                         </div>
+//                     </div>
+//                 )
+//                 }
+//                 default: 
+//                 return <></>
+//         };      
+//     }
+//     return getCard(type);
+    
+// }
+
+// export default Card; 
+
+
+
 import React from "react";
 import styles from "./Card.module.css";
 import { Chip, Tooltip } from "@mui/material";
-import { Link } from "react-router-dom";
 
 function Card({data, type}) {
     const getCard = (type) => {
@@ -10,24 +71,27 @@ function Card({data, type}) {
                 const {image, title, follows, slug, songs} = data;
                 return (
                     <Tooltip title={`${songs.length} songs`} placement="top" arrow>
-                    <Link to={`/album/${slug}`}>
+                    <a href={`/album/${slug}`}>
                     <div className={styles.wrapper}>
                         <div className={styles.card}> 
-                            <img src={image} alt="album" loading="lazy"/>
+                            <img src={image} alt="song" loading="lazy"/>
                             <div className={styles.banner}>
                                 <Chip 
                                 label={`${follows} Follows`}
                                 size="small"
                                 className={styles.chip}
                                 />
-                                </div>
-                                </div>
                                 <div className={styles.titleWrapper}>
                                     <p>{title}</p>
                                 </div>
+                            </div>
+                        </div>
+                        <div className={styles.titleWrapper}>
+                            <p>{title}</p>
+                        </div>
                     </div>
-                </Link>
-            </Tooltip>
+                    </a>
+                    </Tooltip>
                 )
             }
             case "song" :{
